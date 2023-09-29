4a0a7505d3c364381c50b54106a03ed319826490
Oscar Arley Sanchez <oasanchez@grupoasd.com>
2023-09-28 19:02:25 -0500

    Ajuste estilos e inclusión de modal

--- Js/scripts.js
+++ Js/scripts.js
@@ -7,24 +7,25 @@ $(document).ready(function() {
     // Evento de clic para cargar la vista de Alarmas
     $('#cargarAlarmas').click(function(e) {
         e.preventDefault();
-        cargarVistaParcial('alarmas.html');
+        cargarVistaParcial('./alarmas.html');
     });
 
     // Evento de clic para cargar la vista de Medicamentos
     $('#cargarMedicamentos').click(function(e) {
         e.preventDefault();
-        cargarVistaParcial('medicamentos.html');
+        cargarVistaParcial('./medicamentos.html');
     });
 
     //Evento de clic para cargar la vista de crear alarmas
     $(document).on('click', '#btn-crear-alarmas', function(e) {
         e.preventDefault();
-        $('#contenidoCrearAlarma').load('agregarAlarma.html');
+        $('#contenidoCrearAlarma').load('./agregarAlarma.html');
     });
 
     //Evento de clic para cargar la vista de crear medicamentos
     $(document).on('click', '#btn-crear-medicamento', function(e) {
         e.preventDefault();
-        $('#contenidoCrearMedicamento').load('agregarMedicamento.html');
-    });
+        $('#contenidoCrearMedicamento').load('./agregarMedicamento.html');
+    });    
+  
 });
